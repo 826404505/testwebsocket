d6e43863181402d4d79a38db8148b8bd4b3c1b7e
826404505 <826404505@qq.com>
2019-04-22 20:22:57 +0800

    twice update component

--- src/main/java/com/zsq/testwebsocket/component/WebsocketTextHandler.java
+++ src/main/java/com/zsq/testwebsocket/component/WebsocketTextHandler.java
@@ -1,6 +1,29 @@
 package com.zsq.testwebsocket.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+
+@Component
 public class WebsocketTextHandler {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public void handlerTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+
+        //得到message里面的数据
+        String payload = message.getPayload();
+        //打印输出数据
+        logger.info("receive msg: {}", payload);
+
+        //把数据打包成websocket发送出去
+        session.sendMessage(new TextMessage("hi websocket"));
+
+    }
 
 
 }
